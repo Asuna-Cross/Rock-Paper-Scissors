a16763d2f427170ea9dafbb82448aebd44563a8d
Asuna Cross <littlecrystaldragon@gmail.com>
2022-09-01 10:06:00 -0400

    Rock conditions working

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -29,12 +29,24 @@ function getPlayerChoice(checkChoice){
     return playerChoiceUnchanged.toLowerCase();
 }
 
-    /*temp - Show outcome to show proof of working*/
+/*temp - Show outcome to show proof of working*/
 console.log(playChoice);
 
-/*Compared to decide who wins */
+/*******Compared to decide who wins ********/
 
-/*Start if statement - Rock*/
+let winCon;
+
+if (playChoice =="rock"){
+    if(comChoice == "rock"){
+        winCon = "draw"
+    }
+    else if (comChoice == "scissors"){
+        winCon = "win"
+    }
+    else{
+        winCon = "lose"
+    }
+}
     /*If statement Check for a draw*/
     /*Else - Win Statement*/
     /*Else - Loose statement*/
@@ -48,7 +60,7 @@ console.log(playChoice);
     /*Else - Loose statement*/
 /*Else prompt player for different entry*/
 
-
+console.log(winCon);
 /*Give output */
 
 /*Take computer and user choice and outcome of who wins and display */
