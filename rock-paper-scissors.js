036a62c2e069f5467895e6e50d9c4523d7fb254f
Asuna Cross <littlecrystaldragon@gmail.com>
2022-09-01 08:20:37 -0400

    Javascript Pseudocode mapped out

---
+++ rock-paper-scissors.js
@@ -0,0 +1,42 @@
+/*Create function get Computer Choice */
+
+/*Declare computer choice as global variable */
+
+/*Declare Function */
+    /*Declare list of what the computer can choose from */
+    /*Maths module to pick a random number from list size*/
+    /*Use number to pick outcome and add to global variable*/
+    /*temp - Show outcome to show proof of working*/
+/*End function declaration */
+
+/*Get player selection*/
+
+/*Declare player selection as global variable */
+
+/*Declare function*/
+    /*Prompt user for input*/
+    /*Convert input to lowercase*/
+    /*temp - Show outcome to show proof of working*/
+/*End cuntion declaration*/
+
+
+/*Compared to decide who wins */
+
+/*Start if statement - Rock*/
+    /*If statement Check for a draw*/
+    /*Else - Win Statement*/
+    /*Else - Loose statement*/
+/*Else - Scissors*/
+    /*If statement - check for draw*/
+    /*Else - Win Statement*/
+    /*Else - Loose statement*/
+/*Else - Paper*/
+    /*If statement - check for draw*/
+    /*Else - Win Statement*/
+    /*Else - Loose statement*/
+/*Else prompt player for different entry*/
+
+
+/*Give output */
+
+/*Take computer and user choice and outcome of who wins and display */
